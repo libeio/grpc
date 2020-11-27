ca4241f71e192fddd9dbb8a5cc5786bdd3f7ad25
libeio <ytmrlimei@163.com>
2020-11-27 02:36:04 -0800

    modify sth

--- mytest/cmake/modules/Findgrpc.cmake
+++ mytest/cmake/modules/Findgrpc.cmake
@@ -4,7 +4,7 @@ find_library(grpc_LIBRARY grpc HINTS "${PROJECT_SOURCE_DIR}/../build")
 find_path(grpc_INCLUDE grpc/grpc.h HINTS "/usr/local/grpc/include")
 
 if(grpc_LIBRARY AND grpc_INCLUDE)
-    set(ZMQ_FOUND TRUE)
+    set(GRPC_FOUND TRUE)
     
     message(STATUS "${Green}Found grpc library at: ${grpc_LIBRARY}${Reset}")
     message(STATUS "${Green}Found grpc include at: ${grpc_INCLUDE}${Reset}")
